faafe8170b81f2e2bae074cc90cdcff9a4f6fa9b
betonomeshalka06 <anjuta.sizova@gmail.com>
2026-03-04 14:36:47 +0200

    new 1

--- program.py
+++ program.py
@@ -1,3 +1,10 @@
 a=input("Ievadi(kg) betona daudzums")
 b=input("Ievadi(kg) betona daudzums")
 c=input("Ievadi(kg) betona daudzums")
+
+def aprekini_smilt (smilt)
+    a = input("Ievadi(kg) betona daudzums")
+    b = input("Ievadi(kg) betona daudzums")
+    return(a, b)
+
+
